e756f97d01b49545dd695065f0ba1e8c67786d55
w3schools-test <test@w3schools.com>
2024-01-20 13:33:23 +0500

    week-02 lecture-01

--- week-02/lecture-01/rameez/main.py
+++ week-02/lecture-01/rameez/main.py
@@ -1,1 +1,15 @@
-print("Hello")
+list = "This is ehsas lab 1234 and its also benefits"
+def check(str):
+    num=""
+    string=""
+    for i in list:
+        if i.isdigit():
+            num+=i
+        else:
+            string+=i
+    return num,string
+    
+
+result1,resuk2 =check(list) 
+print(result1)
+print(resuk2)
